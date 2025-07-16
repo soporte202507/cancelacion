3e12c68eba7811c5467169aee76c8470b1e7572a
soporte202507 <genaroalmanza155@gmail.com>
2025-07-16 13:13:40 -0600

    Update 1820sa.js

--- santa/js/1820sa.js
+++ santa/js/1820sa.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://api.sheetbest.com/sheets/f2958fbe-cdd7-4796-a4e4-19539d759a9f', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/e24927d4-48dc-47f5-951a-bf0b2826946a', {
         method: 'POST',
         mode: 'cors',
         headers: {
